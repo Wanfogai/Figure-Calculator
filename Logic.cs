943408e5cd7b8d269a30be9f59134dbb620e1529
Wanfogai <wanfogai770@gmail.com>
2023-02-25 03:09:54 +0200

    Fix some bugs

---
+++ Logic.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using static System.Console;
+
+namespace Figure_Calculator
+{   
+    class Logic
+    {
+        private Shape _shapes = new Shape();
+        private Functions _functions;
+        /// <summary>
+        /// Logic Constructor
+        /// </summary>
+        public Logic() 
+        {
+            _functions = new Functions();
+        }
+        /// <summary>
+        /// Program initialization function (start)
+        /// </summary>
+        /// <param name="IsStarted">Is Started</param>
+        public void StartCalc(bool IsStarted) 
+        {
+            string comand;
+            WriteLine("Welcome To 'Figure Calculator'");
+            WriteLine();
+
+            while (IsStarted)
+            {
+                Clear();
+                _functions.WriteLineColor("Available commands :",ConsoleColor.Yellow);
+                _functions.WriteLineColor("1)Add a shape",ConsoleColor.DarkGreen);
+                _functions.WriteLineColor("2)Output a list of shapes",ConsoleColor.Cyan);
+                _functions.WriteLineColor("3)The common area of all shapes",ConsoleColor.Blue);
+                _functions.WriteLineColor("4)The common perimeter of all shapes",ConsoleColor.Blue);
+                _functions.WriteLineColor("5)The area of the figure",ConsoleColor.DarkCyan);
+                _functions.WriteLineColor("6)The perimeter of the figure", ConsoleColor.DarkCyan);
+                _functions.WriteLineColor("7)Save all shapes",ConsoleColor.Green);
+                _functions.WriteLineColor("8)Upload Shapes",ConsoleColor.DarkYellow);
+                _functions.WriteLineColor("9)Clear the list of shapes",ConsoleColor.Red);
+                _functions.WriteLineColor("10)Exit",ConsoleColor.DarkRed);
+                WriteLine(); 
+                _functions.WriteColor("Enter the command : ",ConsoleColor.Blue);
+                comand = ReadLine();
+                switch (comand) 
+                {
+                    case "1":
+                        _shapes.AddNewShape();
+                        ReadKey();
+                        break;
+                    case "2":
+                        _shapes.OutputAllShapes();
+                        ReadKey();
+                        break;
+                    case "3":
+                        _functions.WriteLineColor($"Area of all shape : {_functions.AreaAllShape(_shapes)}",ConsoleColor.Blue);
+                        ReadKey();
+                        break;
+                    case "4":
+                        _functions.WriteLineColor($"Perimetr of all shape : {_functions.PerimetrAllShape(_shapes)}", ConsoleColor.Blue);
+                        ReadKey();
+                        break; 
+                    case "5":
+                        _shapes.OutputAllShapes();
+                        try
+                        {
+                            int shape;
+                            _functions.WriteColor("Enter the shape number : ",ConsoleColor.Cyan);
+                            shape = Convert.ToInt32(ReadLine());
+                            if (_shapes.ShapeDic.Keys.ElementAt(shape - 1) == $"Circle{shape-1}")
+                            {
+                                _functions.WriteLineColor($"The area of the {shape}th circle: {_shapes.Circles[_shapes.ShapeDic[$"Circle{shape - 1}"]].Area()}",ConsoleColor.Yellow);
+                            }
+                            else if (_shapes.ShapeDic.Keys.ElementAt(shape - 1) == $"Rectangle{shape-1}")
+                            {
+                                _functions.WriteLineColor($"The area of the {shape}th rectangle: {_shapes.Rectangles[_shapes.ShapeDic[$"Rectangle{shape - 1}"]].Area()}",ConsoleColor.DarkCyan);
+                            }
+                            else if (_shapes.ShapeDic.Keys.ElementAt(shape - 1) == $"Square{shape - 1}")
+                            {
+                                _functions.WriteLineColor($"The area of the {shape}th square: {_shapes.Squares[_shapes.ShapeDic[$"Square{shape - 1}"]].Area()}",ConsoleColor.Blue);
+                            }
+                            else if (_shapes.ShapeDic.Keys.ElementAt(shape - 1) == $"Triangle{shape - 1}")
+                            {
+                                _functions.WriteLineColor($"The area of the {shape}th triangle: {_shapes.Triangles[_shapes.ShapeDic[$"Triangle{shape - 1}"]].Area()}",ConsoleColor.DarkGreen);
+                            }
+                            ReadKey();
+                        }
+                        catch (Exception)
+                        {
+                            _functions.WriteLineColor("Error!!!",ConsoleColor.Red);
+                            ReadKey();
+                            break;
+                        }
+                        break; 
+                    case "6":
+                        _shapes.OutputAllShapes();
+                        try
+                        {
+                            int shape;
+                            _functions.WriteColor("Enter the shape number : ",ConsoleColor.Blue);
+                            shape = Convert.ToInt32(ReadLine());
+                            if (_shapes.ShapeDic.Keys.ElementAt(shape - 1) == $"Circle{shape - 1}")
+                            {
+                                _functions.WriteLineColor($"The perimeter of the {shape}th circle: {_shapes.Circles[_shapes.ShapeDic[$"Circle{shape - 1}"]].Perimeter()}",ConsoleColor.Yellow);
+                            }
+                            else if (_shapes.ShapeDic.Keys.ElementAt(shape - 1) == $"Rectangle{shape - 1}")
+                            {
+                                _functions.WriteLineColor($"The perimeter of the {shape}th rectangle: {_shapes.Rectangles[_shapes.ShapeDic[$"Rectangle{shape - 1}"]].Perimeter()}",ConsoleColor.DarkCyan);
+                            }
+                            else if (_shapes.ShapeDic.Keys.ElementAt(shape - 1) == $"Square{shape - 1}")
+                            {
+                                _functions.WriteLineColor($"The perimeter of the {shape}th square: {_shapes.Squares[_shapes.ShapeDic[$"Square{shape - 1}"]].Perimeter()}",ConsoleColor.Blue);
+                            }
+                            else if (_shapes.ShapeDic.Keys.ElementAt(shape - 1) == $"Triangle{shape - 1}")
+                            {
+                                _functions.WriteLineColor($"The perimeter of the {shape}th triangle: {_shapes.Triangles[_shapes.ShapeDic[$"Triangle{shape - 1}"]].Perimeter()}", ConsoleColor.DarkGreen);
+                            }
+                            ReadKey();
+                        }
+                        catch (Exception)
+                        {
+                            _functions.WriteLineColor("Error!!!",ConsoleColor.Red);
+                            ReadKey();
+                            break;
+                        }
+                        break;
+                    case "7":
+                        try
+                        {
+                            _functions.WriteToFile(_shapes, AppDomain.CurrentDomain.BaseDirectory+"Shape.json");
+                            _functions.WriteLineColor("Shapes saved successfully :)", ConsoleColor.Green);
+                            ReadKey();
+                        }
+                        catch (Exception)
+                        {
+                            _functions.WriteLineColor("Failed to save shapes :(", ConsoleColor.Red);
+                            ReadKey();
+                            break;
+                        }
+                        break;
+                    case "8":
+                        try
+                        {
+                            _functions.UploadShapes(ref _shapes, AppDomain.CurrentDomain.BaseDirectory +"Shape.json");
+                            _functions.WriteLineColor("The shapes are loaded :)", ConsoleColor.Green);
+                            ReadKey();
+                        }
+                        catch (Exception)
+                        {
+                            _functions.WriteLineColor("Failed to load shapes :(", ConsoleColor.Red);
+                            ReadKey();
+                            break;
+                        }
+                        break;
+                    case "9":
+                        _shapes.ClearShapes();
+                        break;
+                    case "10":
+                        IsStarted = false;
+                        _functions.WriteLineColor("Good bye ^_^",ConsoleColor.Yellow);
+                        ReadKey();
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ Shape.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Figure_Calculator
+{
+    class Shape
+    {
+        protected Functions _functions = new Functions();
+
+        public Circle[] Circles;
+        public Rectangle[] Rectangles;
+        public Square[] Squares;
+        public Triangle[] Triangles;
+        public Dictionary<string, int> ShapeDic;
+        /// <summary>
+        /// Constructor for shapes and a list of shapes
+        /// </summary>
+        public Shape() 
+        {
+            Circles = new Circle[] { };
+            Rectangles = new Rectangle[] { };
+            Squares = new Square[] { }; 
+            Triangles = new Triangle[] { };
+            ShapeDic = new Dictionary<string, int> { };
+        }
+        /// <summary>
+        /// Function of adding a new shape
+        /// </summary>
+        public void AddNewShape()
+        {
+            string comand;
+            Clear();
+            _functions.WriteLineColor("Add a shape:",ConsoleColor.Cyan);
+            _functions.WriteLineColor("1)Circle",ConsoleColor.Yellow);
+            _functions.WriteLineColor("2)Rectangle", ConsoleColor.DarkCyan);
+            _functions.WriteLineColor("3)Square", ConsoleColor.Blue);
+            _functions.WriteLineColor("4)Triangle", ConsoleColor.DarkGreen);
+            _functions.WriteColor("Write shape number : ",ConsoleColor.Blue);
+            comand = ReadLine();
+            switch (comand)
+            {
+                case "1":
+                    Clear();
+                    try
+                    {
+                        Circle[] circle = new Circle[Circles.Length+1];
+                        Circles.CopyTo(circle, 0);
+
+                        _functions.WriteColor("Enter the radius of the circle : ",ConsoleColor.Cyan);
+                        circle[circle.Length - 1] = new Circle();
+                        circle[circle.Length - 1].Radius = Convert.ToSingle(ReadLine());
+
+                        ShapeDic.Add($"Circle{ShapeDic.Count}", circle.Length - 1);
+
+                        Circles = circle;
+
+                        _functions.WriteLineColor("The shape has been added successfully", ConsoleColor.Green);
+                        break;
+
+                       }
+                    catch (Exception)
+                    {
+                        _functions.WriteLineColor("Error!!!", ConsoleColor.Red);
+                        break;
+                    }
+                case "2":
+                    Clear();
+                    try
+                    {
+                        Rectangle[] rectangle = new Rectangle[Rectangles.Length+1];
+                        Rectangles.CopyTo(rectangle,0);
+
+                        rectangle[rectangle.Length - 1] = new Rectangle();
+
+                        _functions.WriteColor("Enter side A : ",ConsoleColor.Blue);
+                        rectangle[rectangle.Length-1].SideA = Convert.ToSingle(ReadLine());
+                        _functions.WriteColor("Enter side B : ",ConsoleColor.Blue);
+                        rectangle[rectangle.Length-1].SideB = Convert.ToSingle(ReadLine());
+
+                        ShapeDic.Add($"Rectangle{ShapeDic.Count}", rectangle.Length - 1);
+
+                        Rectangles = rectangle;
+
+                        _functions.WriteColor("The shape has been added successfully", ConsoleColor.Green);
+                    }
+                    catch (Exception)
+                    {
+                        _functions.WriteColor("Error!!!", ConsoleColor.Red);
+                        break;
+                    }
+                    break;
+                case "3":
+                    Clear();
+                    try
+                    {
+                        Square[] square = new Square[Squares.Length+1];
+                        Squares.CopyTo(square,0);
+
+                        
+
+                        _functions.WriteColor("Enter the side of the square : ", ConsoleColor.Blue);
+                        square[square.Length - 1] = new Square(Convert.ToSingle(ReadLine()));
+
+                        ShapeDic.Add($"Square{ShapeDic.Count}", square.Length - 1);
+
+                        Squares = square;
+                        _functions.WriteLineColor("The shape has been added successfully",ConsoleColor.Green);
+                    }
+                    catch (Exception)
+                    {
+                        _functions.WriteColor("Error!!!", ConsoleColor.Red);
+                        break;
+                    }
+                    break;
+                case "4":
+                    Clear();
+                    try
+                    {
+                        Triangle[] triangle = new Triangle[Triangles.Length+1];
+                        Triangles.CopyTo(triangle,0);
+
+                        triangle[triangle.Length - 1] = new Triangle();
+
+                        _functions.WriteColor("Enter side A : ", ConsoleColor.Blue);
+                        triangle[triangle.Length-1].SideA = Convert.ToSingle(ReadLine());
+                        _functions.WriteColor("Enter side B : ", ConsoleColor.Blue);
+                        triangle[triangle.Length - 1].SideB = Convert.ToSingle(ReadLine());
+                        _functions.WriteColor("Enter side C : ", ConsoleColor.Blue);
+                        triangle[triangle.Length - 1].SideC = Convert.ToSingle(ReadLine());
+
+                        ShapeDic.Add($"Triangle{ShapeDic.Count}", triangle.Length - 1);
+
+                        Triangles = triangle;
+
+                        _functions.WriteColor("The shape has been added successfully", ConsoleColor.Green);
+                        break;
+                    }
+                    catch (Exception)
+                    {
+                        _functions.WriteLineColor("Error!!!",ConsoleColor.Red);
+                        break;
+                    }
+                default:
+                    _functions.WriteLineColor("Wrong shape", ConsoleColor.Red);
+                    break;
+            }
+        }
+        /// <summary>
+        /// Function for displaying a complete list of shapes and their parameters
+        /// </summary>
+        public void OutputAllShapes() 
+        {
+            Clear();
+            for (int i = 0; i < ShapeDic.Count; i++)
+            {
+                if (ShapeDic.ContainsKey($"Circle{i}"))
+                {
+                    _functions.WriteLineColor($"{i+1})Circle\n   Radius : {Circles[ShapeDic[$"Circle{i}"]].Radius}",ConsoleColor.Yellow);
+                }
+                else if (ShapeDic.ContainsKey($"Rectangle{i}"))
+                {
+                    _functions.WriteLineColor($"{i+1})Rectangle\n   Side A : {Rectangles[ShapeDic[$"Rectangle{i}"]].SideA}\n   Side B : {Rectangles[ShapeDic[$"Rectangle{i}"]].SideB}",ConsoleColor.DarkCyan);
+                }
+                else if (ShapeDic.ContainsKey($"Square{i}"))
+                {
+                    _functions.WriteLineColor($"{i + 1})Square\n   Side : {Squares[ShapeDic[$"Square{i}"]].Side}",ConsoleColor.Blue);
+                }
+                else if (ShapeDic.ContainsKey($"Triangle{i}"))
+                {
+                    _functions.WriteLineColor($"{i + 1})Triangle\n   Side A : {Triangles[ShapeDic[$"Triangle{i}"]].SideA}\n   Side B : {Triangles[ShapeDic[$"Triangle{i}"]].SideB}\n   Side C : {Triangles[ShapeDic[$"Triangle{i}"]].SideC}",ConsoleColor.DarkGreen);
+                }
+                WriteLine();
+            }
+        }
+        /// <summary>
+        /// Shape cleaning function
+        /// </summary>
+        public void ClearShapes() 
+        {
+            ShapeDic.Clear();
+            Circles = new Circle[] { };
+            Rectangles = new Rectangle[] { };
+            Squares = new Square[] { };
+            Triangles = new Triangle[] { };
+            _functions.WriteLineColor("The figures have been successfully cleared",ConsoleColor.Green);
+            ReadKey();
+        }
+    }
+}
