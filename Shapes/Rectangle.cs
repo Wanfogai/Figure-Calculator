2f5739e0aa3342b3070328219254dd95333ce4f0
sofia <wanfogai770@gmail.com>
2023-02-27 16:35:07 +0200

    Add renew files

---
+++ Shapes/Rectangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Figure_Calculator
+{
+    class Rectangle
+    {
+        public float SideA { get; set;}
+        public float SideB { get; set;}
+        /// <summary>
+        /// Shape : Rectangle
+        /// </summary>
+        /// <param name="SideA">Side A of the Rectangle</param>
+        /// <param name="SideB">Side B of the Rectangle</param>
+        public Rectangle(float SideA, float SideB) 
+        {
+            this.SideA = SideA;
+            this.SideB = SideB;
+        }
+        /// <summary>
+        /// Shape : Rectangle
+        /// Without parameters
+        /// </summary>
+        public Rectangle() 
+        {
+
+        }
+        /// <summary>
+        /// A function that performs calculations of the area of Rectangles
+        /// </summary>
+        /// <returns>Returns the area of the Rectangle</returns>
+        public float Area() 
+        {
+            return Convert.ToSingle(Round(SideA*SideB,2));
+        }
+        /// <summary>
+        /// The function of finding the perimeter of a Rectangle
+        /// </summary>
+        /// <returns>Returns the perimeter of the Rectangle</returns>
+        public float Perimeter() 
+        {
+            return Convert.ToSingle(Round((SideA+SideB)*2,2));
+        }
+    }
+}
---
+++ Shapes/Triangle.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Math;
+
+namespace Figure_Calculator
+{
+    class Triangle
+    {
+        public float SideA { get; set;}
+        public float SideB { get; set;}
+        public float SideC { get; set;}
+        /// <summary>
+        /// Shape : Triangle
+        /// </summary>
+        /// <param name="SideA">Side A</param>
+        /// <param name="SideB">Side B</param>
+        /// <param name="SideC">Side C</param>
+        public Triangle(float SideA, float SideB, float SideC) 
+        {
+            this.SideA = SideA;
+            this.SideB = SideB;
+            this.SideC = SideC;
+        }
+        /// <summary>
+        /// Shape : Triangle
+        /// Without parameters
+        /// </summary>
+        public Triangle() 
+        {
+
+        }
+        /// <summary>
+        /// A function that performs calculations of the area of triangles
+        /// </summary>
+        /// <returns>Returns the area of the triangle</returns>
+        public float Area() 
+        {
+                if (SideA == SideB && SideB == SideC)
+                {
+                    return Convert.ToSingle(Round((Pow(SideA, 2) * Sqrt(3)) / 4, 2));
+                }
+                else if (SideA == SideB || SideA == SideC || SideB == SideC)
+                {
+                    if (SideA == SideB)
+                    {
+                        return Convert.ToSingle(Round((Convert.ToDouble(SideC) / 4) * Sqrt(4 * Pow(SideA, 2) - Pow(SideC, 2)), 2));
+                    }
+                    else if (SideA == SideC)
+                    {
+                        return Convert.ToSingle(Round((Convert.ToDouble(SideC) / 4) * Sqrt(4 * Pow(SideA, 2) - Pow(SideC, 2)), 2));
+                    }
+                    else
+                    {
+                        return Convert.ToSingle(Round((Convert.ToDouble(SideC) / 4) * Sqrt(4 * Pow(SideA, 2) - Pow(SideC, 2)), 2));
+                    }
+                }
+                else
+                {
+                    float P;
+                    P = (SideA + SideB + SideC) / 2;
+                    return Convert.ToSingle(Round(Sqrt(P * (P - SideA) * (P - SideB) * (P - SideC)), 2));
+                }
+        }
+        /// <summary>
+        /// The function of finding the perimeter of a triangle
+        /// </summary>
+        /// <returns>Returns the perimeter of the triangle</returns>
+        public float Perimeter() 
+        {
+            return Convert.ToSingle(Round(SideA + SideB + SideC,2));
+        }
+
+    }
+}
